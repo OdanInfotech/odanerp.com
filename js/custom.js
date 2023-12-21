d5af5056f77cb20e086e2ad483e9081bdcd9769f
Rohit1415 <bhaturohit549@gmail.com>
2023-12-21 17:40:36 +0530

    working on SEO

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,30 @@
-let isRequested = false;
+document.addEventListener('DOMContentLoaded', () => {
+    const allIndicator = document.querySelectorAll('.accordion_indicator li');
+    const allContent = document.querySelectorAll('.content li');
 
+    allIndicator.forEach(item => item.addEventListener('click', () => {
+        const content = document.querySelector(item.dataset.target);
 
-const requestDemo = () => {
-    isRequested = true
-    console.log('Request button clicked');    
-    
-}
+        allIndicator.forEach(i => i.classList.remove('active'));
+        allContent.forEach(i => i.classList.remove('active'));
 
-if (isRequested === false) {
-    setTimeout(() => {
-        console.log('functon call set time out')
-        $('#exampleModalCenter').modal('show');
-    }, 60000);
-    isRequested = true;
-}
+        content.classList.add('active');
+        item.classList.add('active');
+    }));
+
+    let isRequested = false;
+
+    const requestDemo = () => {
+        isRequested = true;
+        console.log('Request button clicked');
+    };
+
+    if (!isRequested) {
+        setTimeout(() => {
+            console.log('function call set timeout');
+            $('#exampleModalCenter').modal('show');
+        }, 600000000);
+
+        isRequested = true;
+    }
+});
