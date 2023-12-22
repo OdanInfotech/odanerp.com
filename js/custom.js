3e3321a2a97c30b42ea9656ddc3ab3f992f366f7
Rohit1415 <bhaturohit549@gmail.com>
2023-12-22 15:26:03 +0530

    demo form updated

--- js/custom.js
+++ js/custom.js
@@ -23,8 +23,44 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             console.log('function call set timeout');
             $('#exampleModalCenter').modal('show');
-        }, 600000000);
+        }, 3000);
 
         isRequested = true;
     }
 });
+
+
+
+// CONTACK FORM JS
+const formData = {};
+
+function updateFormData(fieldId) {
+    formData[fieldId] = document.getElementById(fieldId).value;
+}
+
+function submitForm() {
+    console.log('Final Form Data:', formData);
+}
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
