ade8fd41a5bfa5c25e7d3b900eaa0c8511984bd6
Rohit1415 <bhaturohit549@gmail.com>
2023-12-23 14:48:35 +0530

    slider added

--- js/custom.js
+++ js/custom.js
@@ -31,7 +31,7 @@ if (isRequested === false) {
     setTimeout(() => {
         console.log('function call set timeout');
         $('#exampleModalCenter').modal('show');
-    }, 60000);
+    }, 600000000);
 
     isRequested = true;
 }
@@ -39,28 +39,78 @@ if (isRequested === false) {
 
 
 // CONTACK FORM JS
-const formData = {};
 
-function updateFormData(fieldId) {
-    formData[fieldId] = document.getElementById(fieldId).value;
-}
+// script.js
 
-function submitForm() {
-    console.log('Final Form Data:', formData);
-}
+$(document).ready(function() {
+    // Create an object to store the form data
+    var formData = {};
+  
+    // Function to update formData on "Designation" change
+    $("#designation").on("change", function() {
+      formData[this.name] = $(this).val();
+    });
+  
+    // Function to update formData on form submission
+    $("#contactForm").submit(function(event) {
+      event.preventDefault();
+  
+      // Iterate through each form field and add it to the object
+      $(this)
+        .find("input, select")
+        .not("#designation") // Exclude Designation from the general form submission
+        .each(function() {
+          formData[this.name] = $(this).val();
+        });
+  
+      // Log the collected data to the console (you can replace this with your desired action)
+      console.log(formData);
+  
+      // Here, you can send the formData object to your server using AJAX or perform other actions as needed
+    });
+  });
+  
 
-(function () {
-    'use strict'
-    var forms = document.querySelectorAll('.needs-validation')
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+
+//   slider
+  document.addEventListener("DOMContentLoaded", function () {
+    var mySwiper = new Swiper('.clients-slider', {
+      slidesPerView: 6,
+      spaceBetween: 80,
+    });
+  });
+
+
+
+//   counter
+  function animateCounter(element, start, end, duration) {
+    let startTime;
+
+    function updateCounter(timestamp) {
+      if (!startTime) startTime = timestamp;
+
+      const elapsed = timestamp - startTime;
+      const progress = Math.min(elapsed / duration, 1);
+
+      const value = Math.floor(start + progress * (end - start));
+      element.textContent = value;
+
+      if (progress < 1) {
+        requestAnimationFrame(updateCounter);
+      }
+    }
+
+    requestAnimationFrame(updateCounter);
+  }
+
+  // Get the counter element
+  const counterElement = document.getElementById('happyClients');
+
+  // Set your counter details
+  const startValue = 0;
+  const endValue = 232;
+  const duration = 1000; // in milliseconds
+
+  // Start the counter animation
+  animateCounter(counterElement, startValue, endValue, duration);
